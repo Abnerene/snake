fcc3b561aeed32bd5a1cc79d4daab5d9f351da8f
Abner Barrios <abnerenergy@gmail.com>
2022-01-08 20:25:16 -0600

    Food logic is working,

--- app/js/game.js
+++ app/js/game.js
@@ -1,19 +1,20 @@
 var myBody;
 var map;
 
-
-function init() {
+async function init() {
   myBody = getEID("myBody");
   map = getEID("map");
-  Map();
+  
+  await Map();
+  newFood()
 }
 
-var limiLine=0;
+var limitLine = 0;
 function Map() {
-  map.innerHTML='';
+  map.innerHTML = "";
   optionsSize = [1, 4, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169];
-  arraySize = optionsSize[8];
-
+  arraySize = optionsSize[5];
+  
   getEID("nBlocks").innerHTML = arraySize + " Blocks";
   s = 0;
   mapHeigth = map.offsetHeight;
@@ -21,7 +22,6 @@ function Map() {
 
   blockSize = mapHeigth / Math.sqrt(arraySize);
   limitLine = Math.sqrt(arraySize);
-  console.log(limitLine);
   var xPos = 0;
   var yPos = 0;
   limit = 0;
@@ -43,6 +43,7 @@ function Map() {
     xPos++;
     s++;
   }
+ 
 }
 
 var Snake = {
@@ -50,10 +51,10 @@ var Snake = {
     x: 0,
     y: 0,
   },
-  head:'block-0-0',
-  tail:'block-0-0',
-  body:['block-0-0'],
-  size:4
+  head: "block-0-0",
+  tail: "block-0-0",
+  body: [],
+  size: 2,
 };
 
 var moveOn = {
@@ -63,58 +64,84 @@ var moveOn = {
   y1: false,
 };
 
-var snakeSize=1;
+var snakeSize = 1;
 function moveSnake() {
   var xPos = Snake.position.x;
   var yPos = Snake.position.y;
-  block = "block-" + xPos + "-" + yPos;
-  
+
   if (moveOn.x0) Snake.position.x = xPos - 1;
   if (moveOn.x1) Snake.position.x = xPos + 1;
   if (moveOn.y0) Snake.position.y = yPos - 1;
   if (moveOn.y1) Snake.position.y = yPos + 1;
-  
-  if(snakeSize >= Snake.size){
-    console.log(Snake.body[0]);
-    resetBlock(Snake.body[0]);
-    Snake.body.shift();  
+
+  if (snakeSize > Snake.size ) {
+    if(Snake.body[0]){
+        console.log(Snake.body[0]);
+        resetBlock(Snake.body[0]);
+        Snake.body.shift();
+    }
   }
 
-  if(xPos+1 > limitLine || yPos+1  > limitLine ){
-    alert('GameOver');
-    startGame(false)
-    resetGame()
-  }else{
-    Snake.tail=Snake.head;
-    block = "block-" + xPos + "-" + yPos;
+  block = "block-" + xPos + "-" + yPos;
+  if (getEID(block)) {
     getEID(block).setAttribute("class", "snakeBlock");
-    Snake.head=block;
-    Snake.body.push(block);
-    // console.log(Snake.head);
+    if(block == Food.block){
+        newFood()
+        snakeSize=Snake.size
+        Snake.size+= 1
+        console.log(Snake.size)
+        console.log(Snake.body[0])
+        console.log(Snake.body)
+    }
+    Snake.body.push( "block-" + xPos + "-" + yPos);
+    snakeSize++;
+
+  } else {
+    alert("GameOver");
+    startGame(false);
+    resetGame();
   }
 
-  snakeSize++
   
 }
 
-function resetBlock(block){
-    block=getEID(block);
-    block.style.background=randomColor();
-    block.setAttribute('class','blockFree');
+var Food={
+    block:''
+};
+function newFood(){
+
+    while(true){
+    xPos=getRandom(limitLine)-1;
+    yPos=getRandom(limitLine)-1;
+    block=getEID('block-'+xPos+'-'+yPos);
+
+        if(block.getAttribute('class')!='snakeBlock' ){
+            block.setAttribute('class','foodBlock');
+            Food.block=block.getAttribute('id');
+            break;
+        }
+    }
+
+}
+
+function resetBlock(block) {
+  block = getEID(block);
+  if(block){
+    block.style.background = randomColor();
+    block.setAttribute("class", "blockFree");
+  }
 }
 
 function controls(value) {
-    // console.log(value);
-    moveOn.x0=false;
-    moveOn.x1=false;
-    moveOn.y0=false;
-    moveOn.y1=false;
-    if(value=="x0")moveOn.x0=true;
-    if(value=="x1")moveOn.x1=true;
-    if(value=="y0")moveOn.y0=true;
-    if(value=="y1")moveOn.y1=true;
-
-  console.log(moveOn);
+  // console.log(value);
+  moveOn.x0 = false;
+  moveOn.x1 = false;
+  moveOn.y0 = false;
+  moveOn.y1 = false;
+  if (value == "x0") moveOn.x0 = true;
+  if (value == "x1") moveOn.x1 = true;
+  if (value == "y0") moveOn.y0 = true;
+  if (value == "y1") moveOn.y1 = true;
 }
 
 function updateMap() {
@@ -131,14 +158,16 @@ function startGame(status) {
   }
 }
 
-function resetGame(){
-    Snake.position.x=0;
-    Snake.position.y=0;
-    Map()
+function resetGame() {
+  Snake.position.x = 0;
+  Snake.position.y = 0;
+  Map();
+  newFood()
 }
 
 
-function randomColor(){
-    bgColor = `${getRandom(2)},${getRandom(0)},${getRandom(255)}`;
-    return(bgColor);
+
+function randomColor() {
+  bgColor = `${getRandom(2)},${getRandom(0)},${getRandom(255)}`;
+  return bgColor;
 }
